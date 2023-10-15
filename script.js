17bbc25aa0911278242873367d0f730303617e1f
Nikolina Kokor <nkokor@icloud.com>
2023-10-15 19:53:17 +0200

    player movement implemented

--- script.js
+++ script.js
@@ -7,18 +7,67 @@ context.fillStyle = 'white'
 context.fillRect(0, 0, canvas.width, canvas.height)
 
 //map creation
-const map = new Image()
-map.src = './assets/world-map.png'
+const mapImage = new Image()
+mapImage.src = './assets/world-map.png'
 
 //player creation
 const player = new Image()
 player.src = './assets/player-down.png'
 
+class Sprite {
+  constructor({
+    position,
+    image
+  }) {
+    this.position = position
+    this.image = image
+  }
+  draw() {
+    context.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
+const map = new Sprite({
+  position: {
+    x: -6500,
+    y: -1850
+  },
+  image: mapImage
+})
+
+const keys = {
+  up: {
+    pressed: false
+  },
+  right: {
+    pressed: false
+  },
+  down: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+}
+
+function movePlayer() {
+  //player movement is implemented by changing the map position
+  if(keys.up.pressed) {
+    map.position.y += 3
+  } else if(keys.right.pressed) {
+    map.position.x -= 3
+  } else if(keys.down.pressed) {
+    map.position.y -= 3
+  } else if(keys.left.pressed) {
+    map.position.x += 3
+  }
+}
+
 function animate() {
   window.requestAnimationFrame(animate)
 
   //map rendering
-  context.drawImage(map, -6500, -1850)
+  map.draw()
 
   //player rendering
   context.drawImage(
@@ -32,22 +81,44 @@ function animate() {
     player.width / 4, //actual width of the image being rendered
     player.height //actual height of the image being rendered
   )
+  movePlayer()
 }
 
 // player direction change
 window.addEventListener('keydown', (event) => {
   switch(event.key) {
     case 'ArrowUp':
+      keys.up.pressed = true
       player.src = './assets/player-up.png'
       break
     case 'ArrowRight':
+      keys.right.pressed = true
       player.src = './assets/player-right.png'
       break
     case 'ArrowDown':
+      keys.down.pressed = true
       player.src = './assets/player-down.png'
       break
     case 'ArrowLeft':
+      keys.left.pressed = true
       player.src = './assets/player-left.png'
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch(event.key) {
+    case 'ArrowUp':
+      keys.up.pressed = false
+      break
+    case 'ArrowRight':
+      keys.right.pressed = false
+      break
+    case 'ArrowDown':
+      keys.down.pressed = false
+      break
+    case 'ArrowLeft':
+      keys.left.pressed = false
       break
   }
 })
