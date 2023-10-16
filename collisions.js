ee23d3e3492587d2bac436e337fcb722c63179f2
Nikolina Kokor <nkokor@icloud.com>
2023-10-16 11:37:28 +0200

    collision blocks added to scene

---
+++ collisions.js
@@ -0,0 +1,35 @@
+class CollisionBlock {
+  static dimension = 56
+  constructor({
+    position
+  }) {
+    this.position = position,
+    this.width = CollisionBlock.dimension,
+    this.height = CollisionBlock.dimension
+  }
+  draw() {
+    context.fillStyle = 'red'
+    context.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+//creating a border map from an array of blocks
+// 140 was the width of the map in Tiled
+const borderMap = []
+for(let i = 0; i < borderBlocks.length; i += 140) {
+  borderMap.push(borderBlocks.slice(i, i + 140))
+}
+
+const collisionBlocks = []
+borderMap.forEach((row, i) => {
+  row.forEach((item, j) => {
+    if(item === 1432) {
+      collisionBlocks.push(new CollisionBlock({
+        position: {
+          x: j * CollisionBlock.dimension + offset.x,
+          y: i * CollisionBlock.dimension + offset.y
+        }
+      }))
+    }
+  })
+})
---
+++ context.js
@@ -0,0 +1,5 @@
+//canvas setup
+const canvas = document.getElementById('game-canvas')
+canvas.width = 1024
+canvas.height = 586
+const context = canvas.getContext('2d')
