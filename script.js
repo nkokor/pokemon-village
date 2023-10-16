edf6b46142efe580022599da7e734ac3260ec95a
Nikolina Kokor <nkokor@icloud.com>
2023-10-16 11:38:32 +0200

    sprites moved to a separate file

--- script.js
+++ script.js
@@ -1,82 +1,4 @@
-//canvas setup
-const canvas = document.getElementById('game-canvas')
-canvas.width = 1024
-canvas.height = 586
-const context = canvas.getContext('2d')
-context.fillStyle = 'white' 
-context.fillRect(0, 0, canvas.width, canvas.height)
-
-//map creation
-const mapImage = new Image()
-mapImage.src = './assets/world-map.png'
-
-//player creation
-const playerImage = new Image()
-playerImage.src = './assets/player-down.png'
-
-class Sprite {
-  constructor({
-    position,
-    image,
-    frames = {
-      max: 1
-    }
-  }) {
-    this.position = position
-    this.image = image
-    this.frames = {...frames, value: 0, framesElapsed: 0}
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max
-      this.height = this.image.height
-    }
-    this.moving = false
-  }
-  draw() {
-    context.drawImage(
-      this.image, 
-      this.frames.value * this.width, //cropping starting point from x
-      0, //cropping starting point from y
-      this.image.width / this.frames.max, //cropping lenght for x
-      this.image.height, //cropping length for y
-      this.position.x, //position x
-      this.position.y, //position y
-      this.image.width / this.frames.max, //actual width of the image being rendered
-      this.image.height, //actual height of the image being rendered
-    )
-    if(this.moving) {
-      if(this.frames.max > 1) {
-        this.frames.framesElapsed += 1
-      }
-      //change the image on every 10th animation frame to "slow down" the animation
-      if(this.frames.framesElapsed % 10 === 0) {
-        if(this.frames.value < this.frames.max - 1) {
-          this.frames.value += 1
-        } else {
-          this.frames.value = 0
-        }
-      }
-    }
-  }
-}
-
-const map = new Sprite({
-  position: {
-    x: -5600,
-    y: -1570
-  },
-  image: mapImage
-})
-
-const player = new Sprite({
-  position: {
-    x: canvas.width / 2 - playerImage.width / 4,
-    y: canvas.height / 2 - playerImage.height / 2
-  },
-  image: playerImage,
-  frames: {
-    max: 4
-  }
-})
+const worldObjects = [map, ...collisionBlocks, foreground]
 
 const keys = {
   up: {
@@ -92,7 +14,6 @@ const keys = {
     pressed: false
   }
 }
-
 let lastPressedKey = ''
 
 function movePlayer(lastPressedKey) {
@@ -100,27 +21,36 @@ function movePlayer(lastPressedKey) {
   //player movement is implemented by changing the map position
   if(keys.up.pressed && lastPressedKey === 'up') {
     player.moving = true
-    map.position.y += 3
+    worldObjects.forEach(object => {
+      object.position.y += 3
+    })
   } else if(keys.right.pressed  && lastPressedKey === 'right') {
     player.moving = true
-    map.position.x -= 3
+    worldObjects.forEach(object => {
+      object.position.x -= 3
+    })
   } else if(keys.down.pressed  && lastPressedKey === 'down') {
     player.moving = true
-    map.position.y -= 3
+    worldObjects.forEach(object => {
+      object.position.y -= 3
+    })
   } else if(keys.left.pressed  && lastPressedKey === 'left') {
     player.moving = true
-    map.position.x += 3
+    worldObjects.forEach(object => {
+      object.position.x += 3
+    })
   }
 }
 
 function animate() {
   window.requestAnimationFrame(animate)
-
-  //map rendering
   map.draw()
-
-  //player rendering
+  collisionBlocks.forEach(block => {
+    block.draw()
+  })
   player.draw()
+  //foreground rendering must be done after player rendering so foreground objects are always in front of the player
+  foreground.draw()
   movePlayer(lastPressedKey)
 }
 
---
+++ sprites.js
@@ -0,0 +1,86 @@
+class Sprite {
+  constructor({
+    position,
+    image,
+    frames = {
+      max: 1
+    }
+  }) {
+    this.position = position
+    this.image = image
+    this.frames = {...frames, value: 0, framesElapsed: 0}
+    //images might take some time to load
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max
+      this.height = this.image.height
+    }
+    this.moving = false
+  }
+  draw() {
+    context.drawImage(
+      this.image, 
+      this.frames.value * this.width, //cropping starting point from x
+      0, //cropping starting point from y
+      this.image.width / this.frames.max, //cropping lenght for x
+      this.image.height, //cropping length for y
+      this.position.x, //position x
+      this.position.y, //position y
+      this.image.width / this.frames.max, //actual width of the image being rendered
+      this.image.height, //actual height of the image being rendered
+    )
+    if(this.moving) {
+      if(this.frames.max > 1) {
+        this.frames.framesElapsed += 1
+      }
+      //change the image on every 10th animation frame to "slow down" the animation
+      if(this.frames.framesElapsed % 10 === 0) {
+        if(this.frames.value < this.frames.max - 1) {
+          this.frames.value += 1
+        } else {
+          this.frames.value = 0
+        }
+      }
+    }
+  }
+}
+
+const offset = {
+  x: -5600,
+  y: -1570
+}
+
+//creating world map
+const mapImage = new Image()
+mapImage.src = './assets/pallet-town.png'
+const map = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y 
+  }, 
+  image: mapImage
+})
+
+//creating player
+const playerImage = new Image()
+playerImage.src = './assets/player-down.png'
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - playerImage.width / 4,
+    y: canvas.height / 2 - playerImage.height / 2
+  },
+  image: playerImage,
+  frames: {
+    max: 4
+  }
+})
+
+//creating foreground
+const foregroundMapImage = new Image()
+foregroundMapImage.scr = './assets/foreground-map.png'
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y 
+  }, 
+  image: foregroundMapImage
+})
